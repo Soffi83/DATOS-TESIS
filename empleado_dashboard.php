f453dbcc455ec50b803fa9b527c202846dac6f01
Soffi83 <aguerosofia83@gmail.com>
2026-05-06 23:47:42 -0300

    Update session role check and add navigation links

--- empleado_dashboard.php
+++ empleado_dashboard.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['rol']) || $_SESSION['rol'] != "empleado") {
+if (!isset($_SESSION['rol']) || $_SESSION['rol'] != "2") {
     header("Location: ../login.php");
     exit();
 }
@@ -10,4 +10,12 @@ if (!isset($_SESSION['rol']) || $_SESSION['rol'] != "empleado") {
 <h1>Panel Empleado</h1>
 <p>Bienvenido <?php echo $_SESSION['usuario']; ?></p>
 
+<a href="../EMPLEADO/ver_turnos_empleado.php">Ver agenda de turnos</a>
+<br>
+<a href="../EMPLEADO/historial_turnos.php">Historial de turnos</a>
+<br>
+<a href="../EMPLEADO/mi_cuenta.php">Mi cuenta</a>
+<br>
+<a href="../EMPLEADO/historial_cliente.php">📊 Historial completo de usuarios</a>
+<br>
 <a href="../SESION/logout.php">Cerrar sesión</a>
